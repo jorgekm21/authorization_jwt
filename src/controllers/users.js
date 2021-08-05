a9afe656fc6c9598417a2d2c188dbf3d5750ee55
Jorge Kassabji <jorgekm21@gmail.com>
2021-08-05 11:48:55 -0400

    Token creation in login function

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -8,7 +8,25 @@ const getAdmin = (req, res) => {
 }
 
 const login = (req, res) => {
-    
+    User.findOne({email:req.body.email}, (err, result) => {
+        if(err){
+            res.send('Ha ocurrido un error: ' + err)
+        } else{
+            if(result) {
+                if(bcrypt.compareSync(req.body.password, result.password)){
+                    jwt.sign({
+                        user:result
+                    }, 'secret-key', (err, token) => {
+                        res.send({token:token})
+                    })
+                } else{
+                    res.send('Clave incorrecta')
+                }
+            } else{
+                res.send('No existe el usuario')
+            }
+        }
+    })
 }
 
 const register = (req, res) => {
