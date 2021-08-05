b16175835a1c6ba59d5ff2e285e9e2d120eefe56
Jorge Kassabji <jorgekm21@gmail.com>
2021-08-05 11:20:37 -0400

    Routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
+const userRoutes = require('./routes/users')
 
 
 require('./connection')
 app.set('port', 3000)
 
 app.use(express.urlencoded({extended: false}))
+app.use(userRoutes)
 
 app.listen(app.get('port'), () => {
     console.log('Aplicacion corriendo en el puerto ' + app.get('port'))
